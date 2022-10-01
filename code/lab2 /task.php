f724be697065c27bf6ce3c080c4b33b3678a0932
Валерий Столбов <valeros@MBP-Valerij-2.Dlink>
2022-10-01 11:20:44 +0200

    lab2: 1 и 2 задания

--- code/lab2 /task.php
+++ code/lab2 /task.php
@@ -1,1 +1,33 @@
 <?php
+
+function firstTask()
+{
+	$veryBadUnclearName = '15 chicken wings';
+
+	$order = &$veryBadUnclearName;
+	$order .= ' with double sauce';
+
+	echo "\nYour order is: $veryBadUnclearName.";
+	echo "\n";
+
+}
+
+function secondTask()
+{
+	$firstArg = 42;
+	echo $firstArg;
+	echo "\n";
+	$secondArg = 33.932;
+	echo $secondArg;
+	echo "\n";
+	echo 1+2+3+4+5+6-9;
+	echo "\n";
+
+	$lastMonth = 1187.23;
+	$thisMonth = 1089.98;
+	echo "difference: " . ($lastMonth - $thisMonth);
+	echo "\n";
+}
+
+firstTask();
+secondTask();
