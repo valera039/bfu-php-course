473e5970ba27953f75affbd80abc199331f9f806
Валерий Столбов <valeros@MBP-Valerij-2.Dlink>
2022-10-16 13:51:55 +0200

    lab2: задачки по функциям

--- code/lab2 /task.php
+++ code/lab2 /task.php
@@ -56,12 +56,15 @@ function fifthTask()
 	echo 'Result: ' . $answer . "\n";
 }
 
+// 14. Математические функции
 function sixthTask()
 {
+	// Работа с %
 	$a = 10;
 	$b = 3;
 	echo $a % $b . "\n";
 
+	// Работа со степенью и корнем
 	$a = 32;
 	$b = 7;
 	$module = $a % $b;
@@ -75,11 +78,162 @@ function sixthTask()
 	}
 
 	$st = pow(2, 10);
+	echo $st . "\n";
+
+	echo sqrt(245) . "\n";
+
+	$result = 0;
+	$array = [4, 2, 5, 19, 13, 0, 10];
+	foreach ($array as $item)
+	{
+		$result += $item**2;
+	}
+
+	echo sqrt($result) . "\n";
+
+	// Работа с функциями округления
+
+	$result = sqrt(379);
+	echo round($result) . "\n";
+	echo round($result, 1) . "\n";
+	echo round($result, 2) . "\n";
+
+	$result = sqrt(587);
+	$array = [];
+	$array['floor'] = floor($result);
+	$array['ceil'] = ceil($result);
+	print_r($array);
+
+	// Работа с min и max
+
+	$array = [4, -2, 5, 19, -130, 0, 10];
+
+	echo 'max number: ' . max($array) .  "\n";
+	echo 'min number: ' . min($array) .  "\n";
+
+	// Работа с рандомом
+	echo random_int(1, 100) . "\n";
+
+	$result = [];
+	$i = 0;
+	while ($i < 10)
+	{
+		$result[] = random_int(100, 999);
+		$i++;
+	}
+	print_r($result);
+
+	// Работа с модулем
+	$a = 387;
+	$b = 549;
+	echo abs($a - $b) . "\n";
+
+	$result = [];
+	$array = [1, 2, -1, -2, 3, -3];
+	foreach ($array as $item)
+	{
+		$result[] = abs($item);
+	}
+	print_r($result);
+
+	// Общее
+	$number = 27;
+	$array = [];
+	for ($i = 1; $i < ($number / 2) + 1; $i++)
+	{
+		if ($number % $i === 0)
+		{
+			$array[] = $i;
+		}
+	}
+	echo "\n";
+	print_r($array);
+
+	$array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	$sum = 0;
+	$counter = 0;
+	foreach ($array as $item)
+	{
+		$counter++;
+		$sum += $item;
+		if ($sum > 10)
+		{
+			break;
+		}
+	}
+
+	echo $counter . "\n";
+}
+
+// 15. Функции
+
+function printStringReturnNumber(int $number): int
+{
+	echo $number . "\n";
+
+	return $number;
+}
+
+function seventhTask()
+{
+	$number = printStringReturnNumber(14);
+
+	echo $number . "\n";
+}
+
+// 16. Функции
+
+function increaseEnthusiasm(string $str): string
+{
+	return $str . '!';
+}
+
+function repeatThreeTimes(string $str): string
+{
+	return $str . $str . $str;
+}
+
+function cut(string $str, int $leaveChar = 10): string
+{
+	return mb_substr($str, 0, $leaveChar);
+}
+
+function eighthTask()
+{
+	echo increaseEnthusiasm('Wow that great') . "\n";
+	echo repeatThreeTimes('Many times ') . "\n";
+	echo increaseEnthusiasm(repeatThreeTimes('Some kind of magic')) . "\n";
+	echo cut('So much long string wow', 5) . "\n";
+
+	$array = [1, 6, 34, 13, 355, 112];
+	array_map(static function($item) {
+		echo $item . "\n";
+	}, $array);
+
+	$number = 2732;
+	while ($number > 9)
+	{
+		$result = 0;
+		$tmp = $number;
+		while ($tmp)
+		{
+			$result += $tmp % 10;
+			$tmp /= 10;
+		}
+
+		$number = $result;
+	}
+
+	echo $number . "\n";
 
 }
+
 
 // firstTask();
 // secondTask();
 // thirdTask();
 // fourthTask();
 // fifthTask();
+// sixthTask();
+// seventhTask();
+eighthTask();
